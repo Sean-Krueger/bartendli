2cc94f411d73e521a02c6f37a96e8c42582e7d80
Sean-Krueger <iokreddit@protonmail.com>
2021-12-09 23:18:08 -0500

    nav setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,10 @@ const navbar = () => {
     return (
         <div className="nav">
             Navbar
+            <ul>
+                <li>make me a drink</li>
+                <li>find drinks by alcohol</li>
+            </ul>
         </div>
     )
 }
