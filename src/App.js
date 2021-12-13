4963ccc0c800dd99cdaae82e97b2d2606d673868
Sean-Krueger <iokreddit@protonmail.com>
2021-12-12 21:02:55 -0500

    setting up browserrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Homepage from './pages/Homepage.js'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
 
   return (
-    <div className="App">
-        <Homepage/>
-        
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={HomePage}/>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -3,7 +3,7 @@ import React, {useState, useffect} from 'react'
 const DrinkList = () => {
 const [drinks, setDrinks] = useState()
     
-
+// useeffect to grab drinks
 return (
         <div>
             
